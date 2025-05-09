d6f1e772e2c6840ed09bfba531fe9ac8cc5adcc2
Alex Correa <alex.correa@acad.ufsm.br>
2025-05-09 11:10:23 -0300

    feat: controller endpoints

--- src/main/java/com/example/cadastroninjas/controller/NinjaController.java
+++ src/main/java/com/example/cadastroninjas/controller/NinjaController.java
@@ -1,9 +1,7 @@
 package com.example.cadastroninjas.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -13,6 +11,36 @@ public class NinjaController {
     @GetMapping("/rotapublica")
     public String rotaPublica() {
         return "essa eh a rota publica";
+    }
+
+    // Create ninja
+    @PostMapping
+    public String createNinja() {
+        return "create ninja";
+    }
+
+    // Read ninja
+    @GetMapping
+    public String readNinjas() {
+        return "read ninja";
+    }
+
+    // Read ninja by id
+    @GetMapping
+    public String readById() {
+        return "read ninja by ID";
+    }
+
+    // Update ninja
+    @PutMapping
+    public String updateNinja() {
+        return "update ninja";
+    }
+
+    // Delete ninja
+    @DeleteMapping
+    public String deleteNinjaById() {
+        return "delete ninja by ID";
     }
 
 }
